d10d7f355d995ceb1ad2eb5cabbb162849130d9f
motaz007 <motaz.theab@gmail.com>
2020-11-25 12:12:51 +0100

    implementing a testbench to write into fifo and then read the written data

---
+++ sync_fifo/fifo_tb.v
@@ -0,0 +1,61 @@
+//defines the time scale of simulation, it will run in steps
+//of 1ns and has a precesion value of 1ps
+`timescale 1ns / 1ps
+module testbench;
+    parameter DATA_WIDTH = 8;
+
+
+    //inputs
+    reg clk;   
+    reg rst;   
+    reg wr_cs; 
+    reg rd_cs;    
+    reg [DATA_WIDTH-1:0] data_in ; 
+    reg rd_en;    
+    reg wr_en;
+
+    //output
+    wire full;
+    wire empty;
+    wire [DATA_WIDTH-1:0] data_out;
+    //instantiae the DUT
+    syn_fifo uut(
+        .clk(clk),
+        .rst(rst),
+        .wr_cs(wr_cs),
+        .rd_cs(rd_cs),
+        .data_in(data_in),
+        .rd_en(rd_en),
+        .wr_en(wr_en),
+        .full(full),
+        .empty(empty),
+        .data_out(data_out)
+    );
+
+    initial begin
+        //initialize inputs
+    
+    clk = 1;       // initial value of clock
+    rst = 0;       // initial value of reset
+     #5  rst = 1;    // Assert the reset
+     #10  rst = 0;   // De-assert the reset
+     #10  data_in = 7;  // input datae
+     #12  wr_cs = 1;  // write chip select
+     #12  wr_en = 1;  // write enable
+     #12  rd_cs = 1;  // read chip select
+     #12  rd_en = 1;  // read enable
+     #20  $finish;      // Terminate simulation
+  end
+       
+       always begin
+            #5 clk = ~clk;
+        end
+
+          // Dump all nets to a vcd file called tb.vcd
+  initial begin
+     $display("-I- VCD dump started...");
+     $dumpfile("tb.vcd");
+     $dumpvars(0,testbench);
+  end
+
+endmodule
