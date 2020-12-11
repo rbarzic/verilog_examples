fc1ed7326fbecb3ba5f47188748b942efcee4f1f
motaz007 <motaz.theab@gmail.com>
2020-12-11 17:19:40 +0100

    switch to EMACS editor

--- uart/uart.v
+++ uart/uart.v
@@ -1,50 +1,44 @@
 
-module uart (
-reset          ,
-clk            ,
-ld_tx_data     ,
-tx_data        ,
-tx_enable      ,
-tx_out         ,
-tx_empty       ,
-uld_rx_data    ,
-rx_data        ,
-rx_enable      ,
-rx_in          ,
-rx_empty
-);
 
-// Port declarations
-input        reset          ;
-input        clk            ;
-input  [8:0] tx_data        ;
-input        tx_enable      ;
-output       tx_out         ;
-input        uld_rx_data    ;
-output [8:0] rx_data        ;
-input        rx_enable      ;
-input        rx_in          ;
-output       rx_empty       ;
+module uart (/*AUTOARG*/
+   // Outputs
+   tx_out,
+   // Inputs
+   reset, txclk, tx_enable, tx_data
+   );
 
-// Internal Variables 
-//TX variables
-reg [8:0] tx_reg;
-reg tx_ready; //Triggeres that the data packet is ready to be sent
-reg tx_parity; //the parity bit of tx package odd parity is used
-reg tx_done;
-reg tx_busy;
-reg [11:0] tx_package;
-reg [3:0] tx_cnt;
+   
 
-//TX states
-parameter [1:0] IDLE         = 2'b00;
-parameter [1:0] LOAD_MODE    = 2'b01;
-parameter [1:0] SHIFT_MODE   = 2'b10;
-reg [1:0] state;
+   /*AUTOINPUT*/
+   input reset;
+   input txclk;
+   input tx_enable;
+   input [8:0] tx_data;
+
+   /*AUTOOUTPUT*/
+   output tx_out;
 
 
+   reg [8:0] tx_reg;
+   reg [8:0] tx_reg;
+   reg tx_ready; //Triggeres that the data packet is ready to be sent
+   reg tx_parity; //the parity bit of tx package odd parity is used
+   reg tx_done;
+   reg tx_busy;
+   reg [11:0] tx_package;
+   reg [3:0]  tx_cnt;
+   
+ 
+   /*AUTOREG*/
+   wire      tx_out;
+  /*AUTOWIRE*/
 
-// UART TX Logic
+   parameter [1:0] IDLE         = 2'b00;
+   parameter [1:0] LOAD_MODE    = 2'b01;
+   parameter [1:0] SHIFT_MODE   = 2'b10;
+   reg [1:0] state;
+
+   // UART TX Logic
 always(@ tx_enable or tx_ready )
   begin: FSM
     
@@ -66,7 +60,7 @@ always(@ tx_enable or tx_ready )
           tx_package = {1'b0,tx_reg,tx_parity,1'b1};
           tx_ready = 1;
             
-          
+   
 
           SHIFT_MODE:
 
@@ -75,65 +69,46 @@ always(@ tx_enable or tx_ready )
         end 
       endcase
 
-    end 
+  end // block: FSM
+   
+   always@(posedge clk)
+     begin: Transmision
+	if(reset)begin
+	   tx_out <= 0;
+	   tx_busy<= 0;
+	else begin
+	   if(tx_ready && state == SEND)
+             tx_out  <= tx_package[tx_cnt]
+			tx_busy <= 1;
+	   else
+             tx_out <= tx_out;
+           tx_busy<= tx_busy;
+	end
+	end
 
+	always@(posedge clk)
+	  begin: counter
+	     if(reset) begin
+		tx_cnt = 0;
+	     end else if (tx_ready && state==SEND)begin
+		if(tx_cnt < 10)
+		  tx_cnt <= tx_cnt+1;
+		else 
+		  tx_cnt <= tx_cnt;
+	     end else
+	       tx_cnt <= tx_cnt;
+	  end
 
-  end
+   
 
-
-always@(posedge clk)
-begin: Transmision
-  if(reset)begin
-    tx_out <= 0;
-    tx_busy<= 0;
-    else begin
-      if(tx_ready && state == SEND)
-        tx_out  <= tx_package[tx_cnt]
-        tx_busy <= 1;
-       else
-        tx_out <= tx_out;
-        tx_busy<= tx_busy;
-  end
-end
-
-always@(posedge clk)
-begin: counter
-if(reset) begin
-  tx_cnt = 0;
-end else if (tx_ready && state==SEND)begin
-  if(tx_cnt < 10)
-    tx_cnt <= tx_cnt+1;
-  else 
-    tx_cnt <= tx_cnt;
-end else
-  tx_cnt <= tx_cnt;
-end
-
-
-always@(posedge clk)
-begin: UART_TX
-  if(reset)
-    state       <= IDLE;
-    tx_reg      <= 9b'000000000;
-    tx_parity   <= 0;
-end else begin
-  
-  if
-  
-  if (tx_ready)begin
-    for 
-    //send
-  end
-  
-  end
-
-  end
+endmodule // uart
+/*
+ Local Variables:
+ verilog-library-directories:(
+ "."
+ )
+ End:
+ */
 
 
 
-// UART RX Logic
-
-
-
-
-endmodule
--- uart/uart_tb.v
+++ uart/uart_tb.v
@@ -1,53 +1,40 @@
 //defines the time scale of simulation, it will run in steps
 //of 1ns and has a precesion value of 1ps
 `timescale 1ns / 1ps
-module testbench;
 
+module uart_tb;
 
-    //inputs
-    reg reset;
-    reg txclk;   
-    reg ld_tx_data;   
-    reg [7:0] tx_data; 
-    reg tx_enable;    
-    reg rxclk;
-    reg uld_rx_data ; 
-    reg rx_enable;    
-    reg rx_in;
+  
+   reg reset;
+   reg txclk;   
+   reg [7:0] tx_data; 
+   reg 	     tx_enable;
+   reg      tx_out;
+  
+   /*AUTOREG*/
+    
 
-    //output
-    wire tx_out;
-    wire tx_empty;
-    wire [7:0] rx_data;
-    wire rx_empty;
+   /*AUTOWIRE*/
 
-    //internal varialbes
-    reg [7:0] data = 10101010;
-    integer i;
-    integer j;
-    //wire send;
+   uart uut(
 
-    //instantiae the DUT
-    uart uut(
-.reset(reset)               ,
-.txclk(txclk)               ,
-.ld_tx_data(ld_tx_data)     ,
-.tx_data(tx_data)           ,
-.tx_enable (tx_enable)      ,
-.tx_out (tx_out)            ,
-.tx_empty(tx_empty)         ,
-.rxclk       (rxclk)        ,
-.uld_rx_data (uld_rx_data)  ,
-.rx_data     (rx_data)      ,
-.rx_enable   (rx_enable)    ,
-.rx_in       (rx_in)        ,
-.rx_empty    (rx_empty)
-);
+	    .reset(reset),
+	    .txclk(txclk),
+	    .ld_tx_data(ld_tx_data)     ,
+	    .tx_data(tx_data)           ,
+	    .tx_enable (tx_enable)      ,
+	    .tx_out (tx_out)            ,
+	    .tx_empty(tx_empty)         ,
+	    .rxclk       (rxclk)        ,
+	    .uld_rx_data (uld_rx_data)  ,
+	    .rx_data     (rx_data)      ,
+	    .rx_enable   (rx_enable)    ,
+	    .rx_in       (rx_in)        ,
+	    .rx_empty    (rx_empty)
+	    );
 
-
-
-
-    task tx;
+	    
+task tx;
       begin
       @(posedge txclk) 
         ld_tx_data = 1;
@@ -111,15 +98,26 @@ module testbench;
       #5 txclk = ~txclk;
   end
 
-  always begin
+/* -----\/----- EXCLUDED -----\/-----
+   always begin
       #10 rxclk = ~rxclk;
   end
+ -----/\----- EXCLUDED -----/\----- */
 
-          // Dump all nets to a vcd file called tb.vcd
+    // Dump all nets to a vcd file called tb.vcd
   initial begin
      $display("-I- VCD dump started...");
      $dumpfile("uart_tb.vcd");
-     $dumpvars(0,testbench);
+     $dumpvars(0,uart_tb);
   end
 
-endmodule
+   
+
+endmodule // uart_tb
+/*
+ Local Variables:
+ verilog-library-directories:(
+ "."
+ )
+ End:
+ */
